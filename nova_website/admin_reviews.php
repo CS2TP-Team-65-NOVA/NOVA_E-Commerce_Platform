cf85df2d3adaf26d6022c0cecdb7bd3b93d249cc
SKathiria <shreyakathiria169@gmail.com>
2026-03-21 12:29:25 +0000

    Update admin_reviews.php

--- nova_website/admin_reviews.php
+++ nova_website/admin_reviews.php
@@ -120,6 +120,10 @@ function renderStars($rating) {
 
         <!-- RIGHT SIDE (role-based) -->
         <div class="nav-right">
+        <button id="theme-toggle" class="theme-toggle" type="button" aria-label="Toggle theme">
+            <span id="theme-icon">🌙</span>
+        </button>
+            
         <?php if (!isset($_SESSION['user_id'])): ?>
 
             <a href="register.php" class="nav-link">Register</a>
@@ -359,6 +363,6 @@ function renderStars($rating) {
 
     </div>
 </footer>
-
+<script src="theme.js"></script>
 </body>
 </html>
